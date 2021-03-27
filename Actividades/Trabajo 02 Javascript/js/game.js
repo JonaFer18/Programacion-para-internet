a029ae1475cdb54c585432fed6190247dd27fc39
JonaFer18 <71918026+JonaFer18@users.noreply.github.com>
2021-03-26 20:22:05 -0600

    Update game.js

--- Actividades/Trabajo 02 Javascript/js/game.js
+++ Actividades/Trabajo 02 Javascript/js/game.js
@@ -29,9 +29,9 @@ function preload ()
 {
     this.load.image("fondo", "assets/clouds.png");
     this.load.image("limite", "assets/limit.png");
-    this.load.spritesheet("Bombexplota", "assets/bomb (2).png", { frameWidth: 46, frameHeight: 55 });
+    this.load.spritesheet("Bombexplota", "assets/bomb2.png", { frameWidth: 46, frameHeight: 55 });
     this.load.spritesheet("personaje", "assets/adventurer-Sheet.png", { frameWidth: 50, frameHeight: 37 });
-    this.load.spritesheet("ground", "assets/bomb (1).png", { frameWidth: 53, frameHeight: 40 });
+    this.load.spritesheet("ground", "assets/bomb1.png", { frameWidth: 53, frameHeight: 40 });
 }
 
 function create ()
